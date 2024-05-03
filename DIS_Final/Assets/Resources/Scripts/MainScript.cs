d607f97682098f332ab4e248722909dbf99df9a5
PablooMS <124464156+PablooMS@users.noreply.github.com>
2024-05-03 19:56:10 +0200

    Queso

---
+++ DIS_Final/Assets/Resources/Scripts/MainScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.IO;
+using UnityEditor;
+
+public class MainScript : MonoBehaviour
+{
+    VisualElement characterPageSelector;
+    VisualElement settingsPageSelector;
+    VisualElement characterPage;
+    VisualElement settingPage;
+
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        characterPageSelector = root.Q<VisualElement>("CharacterMark");
+        settingsPageSelector = root.Q<VisualElement>("SettingsMark");
+
+        characterPageSelector.RegisterCallback<ClickEvent>(ChangeToCharacters);
+        settingsPageSelector.RegisterCallback<ClickEvent>(ChangeToSettings);
+    }
+
+    private void ChangeToSettings(ClickEvent e)
+    {
+
+    }
+    
+    private void ChangeToCharacters(ClickEvent e)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
